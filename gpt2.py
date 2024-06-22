5d4f6b5d2e11ee38f8f33d34419757f1838a3f45
0xdeity <deity916@gmail.com>
2024-06-22 13:12:29 -0300

    init commit

---
+++ gpt2.py
@@ -0,0 +1,11 @@
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+text = "Hi who is Steve Jobbs?"
+
+encoded_input = tokenizer.encode(text, return_tensors='pt')
+output = model.generate(encoded_input, max_length=200, do_sample=True, no_repeat_ngram_size=2, early_stopping=True)
+response = tokenizer.decode(output[0], skip_special_tokens=True)
+
+print(response)
